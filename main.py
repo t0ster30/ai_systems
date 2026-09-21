e9f584c63f42bde2d7b6a0ea3c753499faf8b18e
Степкин Егор Сергеевич <29415403@s.mfua.ru>
2026-09-21 10:27:22 +0300

    copy code

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflwo.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
